edd6a7a2ad87bce9b6ed29f3106f1583c8e24663
Avanigadda Thanmai Sasidhar <atsasidhar@gmail.com>
2025-10-03 23:04:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if not api_key:
 else:
     genai.configure(api_key=api_key)
 
-GEMINI_MODEL = "gemini-1.5-flash"
+GEMINI_MODEL = "gemini-1.5-flash-latest"
 OLLAMA_MODEL = "llama3.1:8b"
 
 # -------------------- Initialize Session --------------------
